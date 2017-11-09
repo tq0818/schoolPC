9c9a631495eee994087a06e9585652bfe72b439f
huanglilin <huanglilin@winshare-edu.com>
2017-11-09 17:12:12 +0800

    定时任务暂时不可用，屏蔽

--- yuxin-school-web/src/main/java/com/yuxin/wx/controller/task/TimerTaskJobBean.java
+++ yuxin-school-web/src/main/java/com/yuxin/wx/controller/task/TimerTaskJobBean.java
@@ -1,53 +1,53 @@
-package com.yuxin.wx.controller.task;
-
-import java.lang.reflect.Method;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Scope;
-import org.springframework.scheduling.quartz.QuartzJobBean;
-import org.springframework.stereotype.Component;
-
-public class TimerTaskJobBean  extends QuartzJobBean {
-    protected final Log logger = LogFactory.getLog(getClass());
-    private String targetObject;
-    private String targetMethod;
-    private ApplicationContext ctx;
-
-    @Override
-    protected void executeInternal(JobExecutionContext context)
-            throws JobExecutionException {
-        try {
-            logger.info("execute [" + targetObject + "] at once>>>>>>");
-            Object otargetObject = ctx.getBean(targetObject);
-            Method m = null;
-
-            try {
-                m = otargetObject.getClass().getMethod(targetMethod, new Class[] {JobExecutionContext.class});
-                m.invoke(otargetObject, new Object[] {context});
-            } catch (SecurityException e) {
-                logger.error(e);
-            } catch (NoSuchMethodException e) {
-                logger.error(e);
-            }
-        } catch (Exception e) {
-            throw new JobExecutionException(e);
-        }
-    }
-
-    public void setApplicationContext(ApplicationContext applicationContext) {
-        this.ctx = applicationContext;
-    }
-
-    public void setTargetObject(String targetObject) {
-        this.targetObject = targetObject;
-    }
-
-    public void setTargetMethod(String targetMethod) {
-        this.targetMethod = targetMethod;
-    }
-}
+//package com.yuxin.wx.controller.task;
+//
+//import java.lang.reflect.Method;
+//
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
+//import org.quartz.JobExecutionContext;
+//import org.quartz.JobExecutionException;
+//import org.springframework.beans.factory.config.BeanDefinition;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.Scope;
+//import org.springframework.scheduling.quartz.QuartzJobBean;
+//import org.springframework.stereotype.Component;
+//
+//public class TimerTaskJobBean  extends QuartzJobBean {
+//    protected final Log logger = LogFactory.getLog(getClass());
+//    private String targetObject;
+//    private String targetMethod;
+//    private ApplicationContext ctx;
+//
+//    @Override
+//    protected void executeInternal(JobExecutionContext context)
+//            throws JobExecutionException {
+//        try {
+//            logger.info("execute [" + targetObject + "] at once>>>>>>");
+//            Object otargetObject = ctx.getBean(targetObject);
+//            Method m = null;
+//
+//            try {
+//                m = otargetObject.getClass().getMethod(targetMethod, new Class[] {JobExecutionContext.class});
+//                m.invoke(otargetObject, new Object[] {context});
+//            } catch (SecurityException e) {
+//                logger.error(e);
+//            } catch (NoSuchMethodException e) {
+//                logger.error(e);
+//            }
+//        } catch (Exception e) {
+//            throw new JobExecutionException(e);
+//        }
+//    }
+//
+//    public void setApplicationContext(ApplicationContext applicationContext) {
+//        this.ctx = applicationContext;
+//    }
+//
+//    public void setTargetObject(String targetObject) {
+//        this.targetObject = targetObject;
+//    }
+//
+//    public void setTargetMethod(String targetMethod) {
+//        this.targetMethod = targetMethod;
+//    }
+//}
--- yuxin-school-web/src/main/java/com/yuxin/wx/controller/task/TimerTaskPrimary.java
+++ yuxin-school-web/src/main/java/com/yuxin/wx/controller/task/TimerTaskPrimary.java
@@ -1,77 +1,77 @@
-package com.yuxin.wx.controller.task;
-
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.yuxin.wx.api.company.ICompanyServiceStaticService;
-import com.yuxin.wx.api.course.IVideoService;
-import com.yuxin.wx.api.system.ISysTaskLogService;
-import com.yuxin.wx.common.CCVideoConstant;
-import com.yuxin.wx.company.mapper.CompanyPayConfigMapper;
-import com.yuxin.wx.company.mapper.CompanyServiceStaticDayMapper;
-import com.yuxin.wx.company.mapper.CompanyServiceStaticMapper;
-import com.yuxin.wx.model.company.CompanyPayConfig;
-import com.yuxin.wx.model.company.CompanyServiceStatic;
-import com.yuxin.wx.model.company.CompanyServiceStaticDay;
-import com.yuxin.wx.model.system.SysFileConvertTask;
-import com.yuxin.wx.model.system.SysTaskLog;
-import com.yuxin.wx.scheduled.*;
-import com.yuxin.wx.system.mapper.SysFileConvertTaskMapper;
-import com.yuxin.wx.utils.*;
-import com.yuxin.wx.utils.FileUtil;
-import org.apache.log4j.Logger;
-import org.quartz.JobExecutionContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import sun.net.ftp.FtpClient;
-
-import java.io.*;
-import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.zip.GZIPInputStream;
-
-public class TimerTaskPrimary {
-    @Autowired
-    private ISysTaskLogService sysTaskLogServiceImpl;
-    @Autowired
-    private TikuStatisticsTask tikuStatisticsTask;
-
-    private Logger log = Logger.getLogger(getClass());
-
-    /**
-     * 统计用户答题结果（只针对单选和多选）
-     *
-     */
-    public void taskTikuUserExerciseAnswer(JobExecutionContext context) {
-        SysTaskLog stl = new SysTaskLog();
-        try {
-            stl.setExecuteDate(new Date());
-            stl.setStartTime(new Date());
-            stl.setTaskName("用户课后练习试卷统计");
-            stl.setOperator(0);
-            stl.setOperateTime(new Date());
-            log.info("用户课后练习试卷统计任务-----执行时间：" + new Date());
-            tikuStatisticsTask.tikuStatistics();
-            log.info("用户课后练习试卷统计任务-----处理：完成");
-            stl.setEndTime(new Date());
-            stl.setResult("统计成功");
-            stl.setErrorLog("无错误");
-        } catch (Exception e) {
-            // TODO: handle exception
-            e.printStackTrace();
-            log.info("用户课后练习试卷统计-----异常：" + e.getMessage());
-            stl.setEndTime(new Date());
-            stl.setResult("统计中出错");
-            stl.setErrorLog(e.getMessage());
-        } finally {
-            sysTaskLogServiceImpl.insert(stl);
-        }
-    }
-}
+//package com.yuxin.wx.controller.task;
+//
+//import com.alibaba.fastjson.JSONArray;
+//import com.alibaba.fastjson.JSONObject;
+//import com.yuxin.wx.api.company.ICompanyServiceStaticService;
+//import com.yuxin.wx.api.course.IVideoService;
+//import com.yuxin.wx.api.system.ISysTaskLogService;
+//import com.yuxin.wx.common.CCVideoConstant;
+//import com.yuxin.wx.company.mapper.CompanyPayConfigMapper;
+//import com.yuxin.wx.company.mapper.CompanyServiceStaticDayMapper;
+//import com.yuxin.wx.company.mapper.CompanyServiceStaticMapper;
+//import com.yuxin.wx.model.company.CompanyPayConfig;
+//import com.yuxin.wx.model.company.CompanyServiceStatic;
+//import com.yuxin.wx.model.company.CompanyServiceStaticDay;
+//import com.yuxin.wx.model.system.SysFileConvertTask;
+//import com.yuxin.wx.model.system.SysTaskLog;
+//import com.yuxin.wx.scheduled.*;
+//import com.yuxin.wx.system.mapper.SysFileConvertTaskMapper;
+//import com.yuxin.wx.utils.*;
+//import com.yuxin.wx.utils.FileUtil;
+//import org.apache.log4j.Logger;
+//import org.quartz.JobExecutionContext;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.core.io.ClassPathResource;
+//import org.springframework.core.io.Resource;
+//import org.springframework.core.io.support.PropertiesLoaderUtils;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//import sun.net.ftp.FtpClient;
+//
+//import java.io.*;
+//import java.math.BigDecimal;
+//import java.text.SimpleDateFormat;
+//import java.util.*;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+//import java.util.zip.GZIPInputStream;
+//
+//public class TimerTaskPrimary {
+//    @Autowired
+//    private ISysTaskLogService sysTaskLogServiceImpl;
+//    @Autowired
+//    private TikuStatisticsTask tikuStatisticsTask;
+//
+//    private Logger log = Logger.getLogger(getClass());
+//
+//    /**
+//     * 统计用户答题结果（只针对单选和多选）
+//     *
+//     */
+//    public void taskTikuUserExerciseAnswer(JobExecutionContext context) {
+//        SysTaskLog stl = new SysTaskLog();
+//        try {
+//            stl.setExecuteDate(new Date());
+//            stl.setStartTime(new Date());
+//            stl.setTaskName("用户课后练习试卷统计");
+//            stl.setOperator(0);
+//            stl.setOperateTime(new Date());
+//            log.info("用户课后练习试卷统计任务-----执行时间：" + new Date());
+//            tikuStatisticsTask.tikuStatistics();
+//            log.info("用户课后练习试卷统计任务-----处理：完成");
+//            stl.setEndTime(new Date());
+//            stl.setResult("统计成功");
+//            stl.setErrorLog("无错误");
+//        } catch (Exception e) {
+//            // TODO: handle exception
+//            e.printStackTrace();
+//            log.info("用户课后练习试卷统计-----异常：" + e.getMessage());
+//            stl.setEndTime(new Date());
+//            stl.setResult("统计中出错");
+//            stl.setErrorLog(e.getMessage());
+//        } finally {
+//            sysTaskLogServiceImpl.insert(stl);
+//        }
+//    }
+//}

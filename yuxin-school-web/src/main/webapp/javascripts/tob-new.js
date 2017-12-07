22194853fc82001ea3295d1484283b1746fdc35c
jishangyang <jsy010309@163.com>
2017-12-07 15:15:07 +0800

    冲突

--- yuxin-school-web/src/main/webapp/javascripts/tob-new.js
+++ yuxin-school-web/src/main/webapp/javascripts/tob-new.js
@@ -1,13 +1,11 @@
 // 分校首页，创建分校弹窗
 $(function () {
+
     //点击创建分校弹窗弹框
     $('.createBranch').click(function () {
         $('.popupContainer').show();
         $('.popupOpacity').show();
     });
-    //点击确定关闭弹窗
-    
-
     //点击取消关闭弹窗
     $('.submitSchoolCancel').click(function () {
         $('.popupContainer').hide();
@@ -77,6 +75,24 @@ function addBerkeley(){
    	        	}
    	        }
    	    });
-      
-   
-}
+	}
+//    点击向上向下箭头变换并排序
+$('.sorting').click(function(){
+    if($(this).hasClass('unsort')){
+        $(this).removeClass('unsort');
+        $(this).addClass('iconUp');
+        $(this).html('&#xe68d;');
+
+    }else if($(this).hasClass('iconUp')){
+        $(this).removeClass('iconUp');
+        $(this).addClass('iconDown');
+        $(this).html('&#xe68e;');
+    }else {
+        $(this).removeClass('iconDown');
+        $(this).addClass('unsort');
+        $(this).html('&#xe612;');
+    }
+});
+
+
+});
